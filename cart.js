5ca67489c3b16a0217168a4c4665b669c3a63428
Natalie <natchc@gmail.com>
2024-06-13 20:40:03 +0100

    change class properties to private properties

--- cart.js
+++ cart.js
@@ -1,13 +1,17 @@
 class Cart {
+  #items;
+  #prices;
+  #bulkPrices;
+
   constructor() {
-    this.items = {};
-    this.prices = {
+    this.#items = {};
+    this.#prices = {
       A: 50,
       B: 35,
       C: 25,
       D: 12,
     };
-    this.bulkPrices = {
+    this.#bulkPrices = {
       A: {
         quantity: 3,
         price: 140,
@@ -20,28 +24,29 @@ class Cart {
   }
   addItem(code, quantity) {
     if (quantity <= 0) return;
-    if (this.prices[code]) {
-      if (this.items[code]) {
-        this.items[code] += quantity;
+    if (this.#prices[code]) {
+      if (this.#items[code]) {
+        this.#items[code] += quantity;
       } else {
-        this.items[code] = quantity;
+        this.#items[code] = quantity;
       }
     }
   }
 
   calculateSubtotal() {
     let subtotal = 0;
-    for (const code in this.items) {
-      const count = this.items[code];
-      if (this.bulkPrices[code]) {
-        const bulk = this.bulkPrices[code];
+    for (const code in this.#items) {
+      const count = this.#items[code];
+      if (this.#bulkPrices[code]) {
+        const bulk = this.#bulkPrices[code];
         subtotal += Math.floor(count / bulk.quantity) * bulk.price;
-        subtotal += (count % bulk.quantity) * this.prices[code];
+        subtotal += (count % bulk.quantity) * this.#prices[code];
       } else {
-        subtotal += count * this.prices[code];
+        subtotal += count * this.#prices[code];
       }
     }
     return subtotal;
   }
 }
 module.exports = Cart;
+
